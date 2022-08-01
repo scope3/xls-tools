fecad9cb5be63aff8b4bae806b7c69d66964f8cf
Brandon Kuczenski <brandon.kuczenski@301south.net>
2022-08-01 15:37:37 -0700

    Trial version 0.1.1 with rename

--- setup.py
+++ setup.py
@@ -10,10 +10,13 @@ Version History:
 
 0.1.0  - 8 April 2022 - initial public release
 
+0.1.1 - 1 August 2022 - add CSV reader (with pandas emulation!)
+        change package name to xlstools
+
 """
 
 
-VERSION = '0.1.0'
+VERSION = '0.1.1'
 
 setup(
     name="xlstools",
